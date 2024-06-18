7ed5897099b497f5945a11e9afb9480dc85e7040
fl4me64 <119824603+fl4me64@users.noreply.github.com>
2024-06-18 16:45:15 -0700

    Update views.py
    Add method for listing tweets.

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,18 @@
+import random
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 10)} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
