a5e6abbce6a57c2cfc630c2ff716f01a3ed6776d
fl4me64 <119824603+fl4me64@users.noreply.github.com>
2024-07-27 16:57:42 -0700

    Update views.py
    Updated import statements, added a view for creating tweets, and updated the list view for tweets to use a serializer.

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,41 @@
-import random
+from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import url_has_allowed_host_and_scheme
+from .forms import TweetForm
 from .models import Tweet
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_create_view(request, *args, **kwargs):
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if (request.headers.get('x-requested-with') == 'XMLHttpRequest'):
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = user
+        obj.save()
+        if(request.headers.get('x-requested-with') == 'XMLHttpRequest'):
+            return JsonResponse(obj.serialize(), status=201)
+        if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    if form.errors:
+        if(request.headers.get('x-requested-with') == 'XMLHttpRequest'):
+            return JsonResponse(form.errors, status=400)
+    return render(request, 'components/form.html', context={"form": form})
+
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 10)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "response": tweets_list
     }
